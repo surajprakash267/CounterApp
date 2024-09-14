f3d3fa3d0e7562a95c035f765b3a13deb7e4e799
Suraj Prakash <surajprakash267@gmail.com>
2024-09-15 01:20:49 +0530

    Updated the version of Jetpack Compose ViewModel to 2.8.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     // Jetpack Compose ViewModel integration
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
